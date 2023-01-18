2e9744dcd4b029a1aed6879132586f85ecac6c98
Tomi Hänninen <tomi.hanninen97@gmail.com>
2023-01-18 14:29:26 +0200

    Laskin2

--- Web_Ohjelmointi/Laskin_Harkka/Script/script2.js
+++ Web_Ohjelmointi/Laskin_Harkka/Script/script2.js
@@ -1,13 +1,3 @@
-// function clearFunc(div){
-//     document.getElementById("text1").innerHtml = "";
-// }
-
-// function bPush(id, num){
-//     if (id.contains("click")){
-//         text.innerHTML += num
-//     }
-// }
-
 function findButton(id){
     return document.getElementById(id);
 }
@@ -20,85 +10,38 @@ let opers = [];
 
 
 function calculator(){
-
-    buttons.forEach(function(button){
-        button.addEventListener("click", (e)=>{
-            console.log("test")
-            if(clear.contains(e.target)){
+    for(let i=0; i < buttons.length; i++){
+        buttons[i].addEventListener("click", function(){
+            value = this.innerHTML;
+            if(text.innerHTML === "" || text.innerHTML == "0"){
+                text.innerHTML = value;
+            }
+            else{
+                text.innerHTML += value;
+            }
+            if (value === "Clear"){
                 text.innerHTML = "";
                 nums.length = 0;
                 opers.length = 0;
             }
-
-            if(b1.contains(e.target)){
-                text.innerHTML += "1";
-            }
-
-            if(b2.contains(e.target)){
-                text.innerHTML += "2";
-            }
-
-            if(b3.contains(e.target)){
-                text.innerHTML += "3";
-            }
-
-            if(b4.contains(e.target)){
-                text.innerHTML += "4";
-            }
-
-            if(b5.contains(e.target)){
-                text.innerHTML += "5";
-            }
-
-            if(b6.contains(e.target)){
-                text.innerHTML += "6";
-            }
-
-            if(b7.contains(e.target)){
-                text.innerHTML += "7";
-            }
-
-            if(b8.contains(e.target)){
-                text.innerHTML += "8";
-            }
-
-            if(b9.contains(e.target)){
-                text.innerHTML += "9";
-            }
-
-            if(b0.contains(e.target)){
-                text.innerHTML += "0";
-            }
-
-            if(bCom.contains(e.target)){
-                text.innerHTML += ".";
-            }
-
-            if(bSum.contains(e.target)){
-                nums.push(parseInt(text.innerHTML));
-                opers.push("+");
+            if(value === "X" || value === "/" || value === "+" || value === "-"){
+                nums.push(parseFloat(text.innerHTML));
                 text.innerHTML = "";
+                nums.push(value);
             }
-
-            if(bMinus.contains(e.target)){
-                nums.push(parseInt(text.innerHTML));
-                opers.push("-");
-                text.innerHTML = "";
-            }
-
-            if(bEqv.contains(e.target)){
-                nums.push(parseInt(text.innerHTML));
-                nums.length = 0;
-                opers.length = 0;
-                console.log(nums[0]);
-            }
-            
-            if(bDiv.contains(e.target)){
+            if(value === "="){
+                nums.push(parseFloat(text.innerHTML));
+                let sum = nums.join(" ");
+                let endSum = 
+                text.innerHTML = sum;
+                console.log(eval(sum))
                 console.log(nums)
-                console.log(opers)
+                console.log(sum);
+                // console.log(opers);
+                // console.log(2+2*3)
             }
         })
-    })
+    }
 }
 
 calculator()
